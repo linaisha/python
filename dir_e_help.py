077258566b597334511501b470ca4b4b87fcca15
Elisa de Oliveira Martins <62621575+linaisha@users.noreply.github.com>
2020-12-25 19:28:29 -0300

    Add files via upload

---
+++ dir_e_help.py
@@ -0,0 +1,13 @@
+"""
+Utilitário Python para te auxiliar na programação.
+
+dir -> Apresenta todos os atributos/propiedades e funções/métodos disponíveis para determinado tipo de dado
+ou variável.
+
+dir(tipo de dado/variável)
+
+help -> Apresenta a documentação/como utilizar os atributos/propiedades e funções/métodos disponíveis para determinado
+tipo de dado ou variável.
+
+help(tipo de dado/variável.propriedade)
+"""
